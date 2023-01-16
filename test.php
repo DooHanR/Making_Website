a00086fedf7038f5a84a0cd6d61e2f016272dcad
dhe <dhe_crysis@naver.com>
2023-01-16 14:58:46 +0900

    php 공부중..

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
 <?php
-    echo "hello world";
+    echo "hello world\n<br>"; // semicolon이 반드시 들어가야함.
+    
+    /* 
+    여러줄 주석 만들기
+    아이우에오.
+    */
+
+    // 한줄 주석 만들기 
+
+    $x += 10;
+    $mycounter = 1;
+    $mystring = "Hello";
+    $myarray = array("One", "Two", "Three");
+
+    /* '$' 기호는 변수를 의미한다! 
+    그리고 python 처럼 엄격하게 지켜야할 규칙이
+    없는 편이긴하지만 지키는게 가독성등에 도움이 되긴 한다. */
+
+    $username = "Fred Smith\n<br>";
+    echo $username;
+    $current_user = $username;
+    echo $current_user;
 ?>
