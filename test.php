b0aeb97653b95a13c7dfd3d5017ef90d176a1aca
dhe <dhe_crysis@naver.com>
2023-01-17 09:27:01 +0900

    재미있는 php 공부

--- test.php
+++ test.php
@@ -1,25 +1,34 @@
 <?php
-    echo "hello world\n<br>"; // semicolon이 반드시 들어가야함.
+    // semicolon 반드시 사용.
+    echo "hello world\n<br>";
     
     /* 
-    여러줄 주석 만들기
-    아이우에오.
+    여러줄 주석
+    가나다라
+    마바사아
+    자카파하
     */
 
-    // 한줄 주석 만들기 
+    // 한줄 주석 
 
+    // '$' symbole : variable 의미.
     $x += 10;
     $mycounter = 1;
     $mystring = "Hello";
     $myarray = array("One", "Two", "Three");
 
-    /* '$' 기호는 변수를 의미한다! 
-    그리고 python 처럼 엄격하게 지켜야할 규칙이
+    /* '$' python 처럼 엄격하게 지켜야할 규칙이
     없는 편이긴하지만 지키는게 가독성등에 도움이 되긴 한다. */
 
+    // Variables
+
+    // string variables
     $username = "Fred Smith\n<br>";
     echo $username;
     $current_user = $username;
     echo $current_user;
     
+    // numeric variables
+    $count = 17.5;
+    echo $count;
 ?>
