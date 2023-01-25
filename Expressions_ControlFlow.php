7a8ceddaf26023dd5df2e5ca341dcad37f48a195
dhe <dhe_crysis@naver.com>
2023-01-25 17:04:27 +0900

    expression이 표현식 말하는건가? 수식 같은거 나오는? 잘모르겠다.

---
+++ Expressions_ControlFlow.php
@@ -0,0 +1,19 @@
+<?php
+
+    /* 이곳에서는 PHP 프로그래밍이 실제로 어떻게 작동하고
+    program 의 flow을 어떻게 컨트롤 하는지에 대해 공부할 것이다. */
+
+
+    /* Expressions 
+     표현식은 값, 변수, 연산자, 함수의 조합을 통해 값을 return 하는 것이다.
+    ex) y = 3 (|2x| + 4) 와 같이 말이다.*/
+    
+    
+    /* TRUE OR FALSE? 
+     */
+
+    echo "a: [". TRUE . "]<br>"; // 1이 출력.
+    echo "b: [". FALSE . "]<br>"; // PHP에서 FALSE는 NULL. 미출력.
+    echo "c: [". (1==0) . "]<br>";
+    echo "d: [". (1==1) . "]<br>";
+?>
