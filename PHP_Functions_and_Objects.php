50efc6f144c41634cf414c053d0759eb215d6dc5
dhe <dhe_crysis@naver.com>
2023-02-14 16:54:21 +0900

    재미있는 function, object 공부

--- PHP_Functions_and_Objects.php
+++ PHP_Functions_and_Objects.php
@@ -1,6 +1,68 @@
-<php
+<?php
+
+    /* 5. PHP Functions and Objects 
+     여기서는 다른 프로그래밍 언어에서도 배웠던 함수, 객체에 대해 공부해볼 것입니다.
+    여기서 객체는 함수와 함수가 사용하는 데이터를 클래스라는 구조내에 통합한것을 의미합니다.
+    함수는 일전의 것들과 마찬가지로 특정 작업을 하는 코드를 모듈화 한것이라 보면 됩니다. 
+    
+    이 챕터에서는 함수, 그리고 객체에 대해 공부해볼것이다. */
+
+    echo "hello this is Functions, Object Practice page.<br><br>";
+    
+
+    
+    /* 5.1 PHP Functions */
+
+    // echo date("l");
+    // echo phpinfo(); // 매우 유용한 정보지만 해커에게도 유용 따라서 유의해라.
+    echo strrev(" .dlrow olleH") . "<br>"; // Reverse String
+    echo str_repeat("Hip ", 2) . "<br>"; // Repeat String
+    echo strtoupper("hooray!") . "<br>"; // String to uppercase
+    
+
+    /* 5.1.1 Defining a Function 
+     여타의 언어들과 유사하다. */
+
+    // function function_name([parameter])
+    // {
+    //      Statesment;
+    // }
 
 
+    /* 5.1.2 Returning a Value */
+
+
+    /* 5.1.3 Returning an Array */
+    
+    
+    /* 5.1.4 Passing Arguments by Reference */
+
+
+    /* 5.1.5 Returning Global Variables */
+
+
+    /* 5.1.6 Recap of Variable Scope */
+
+
+
+    /* 5.2 Including and Requiring Files */
+  
+
+    /* 5.2.1 */
+  
+  
+
+    /* 5.3 PHP Version Compatibility*/
+  
+
+    /* 5.3.1 */
+  
+
+
+    /* 5.4 PHP Objects*/
+
+
+    /* 5.4.1 */
 
 
 ?>
