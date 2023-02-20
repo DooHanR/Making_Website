5bb8d8075b14989457ad7205cb079a1d8ac831c3
dhe <dhe_crysis@naver.com>
2023-02-20 17:43:01 +0900

    php Array 공부를 위해 간략히 정리했다.

--- PHP_06_Arrays.php
+++ PHP_06_Arrays.php
@@ -3,5 +3,27 @@
     
     echo "Hello. this pag is for studying PHP Arrays. <br><br>";
 
+    /* 6.1 Basic Access */
+    /* 6.1.1 Numerically indexed Arrays */
+    /* 6.1.2 Associative Arrays */
+    /* 6.1.3 Assignment Using the array Keyword */
+
+
+    /* 6.2 The foreach...as Loop */
+
+
+    /* 6.3 Multidimensional Arrays */
+
+
+    /* 6.4 Using array Functions */
+    /* 6.4.1 is_array */
+    /* 6.4.2 count */
+    /* 6.4.3 sort */
+    /* 6.4.4 shuffle */
+    /* 6.4.5 explode */
+    /* 6.4.6 extract */
+    /* 6.4.7 Using compact */
+    /* 6.4.8 rest */
+    /* 6.4.9 end */
 
 ?>
