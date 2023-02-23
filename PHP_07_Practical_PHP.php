a52872ff87c981c3f3ace7819a32114cb057c7dd
dhe <dhe_crysis@naver.com>
2023-02-23 17:15:49 +0900

    퇴근전까지의 php 실제 응용 공부.

---
+++ PHP_07_Practical_PHP.php
@@ -0,0 +1,94 @@
+<?php
+    
+    printf("This is page for Pracitcal PHP studying. <br><br>");
+
+    /* 7. Practical PHP 
+     앞서 배운것들은 기본적인 php의 구성요소들이 였다. 
+    여기서부터는 문자열 다루는 법, 시간표시, 파일 관리 등에대해 공부할 것. */
+
+
+    /* 7.1 Using printf 
+     c언어 에서 쓰던것과 유사한데 $d 와 같은 매개변수를 통해 출력 포맷을
+    지정해줄 수 있다. 예시를 보자. */
+
+    // printf("There are $d items in your basket", 3);
+
+    /* 적절한 '%' symbol 과 양식을 잘 맞춘다면 뭐든 출력 가능하다. */
+
+    printf("My name is %s. I'm %d years old, which is %X in hexadecimal<br>",
+           'Simon', 33, 33);
+
+    /* printf 로 색 부여하기. */
+
+    printf("<span style='color:#%X%X%X'>Hello<br></span>", 65, 127, 245);
+
+
+    /* 7.1.1 Precision Setting */
+
+
+    /* 7.1.2 String Padding */
+
+
+    /* 7.1.3 Using sprintf */
+
+
+
+    /* 7.2 Date and Time Functions */
+
+
+    /* 7.2.1 Date Constants */
+
+
+    /* 7.2.2 Using Checkdate */
+
+
+
+    /* 7.3 File Handling */
+
+
+    /* 7.3.1 Checking Whetere a File exists*/
+
+
+    /* 7.3.2 Creating a File */
+
+
+    /* 7.3.3 Reading from Files */
+
+
+    /* 7.3.4 Copying Files */
+
+
+    /* 7.3.5 Moving a File */
+
+
+    /* 7.3.6 Deleting a File */
+
+
+    /* 7.3.7 Updating Files */
+
+
+    /* 7.3.8 Locking Files for Multiple Accesses */
+
+
+    /* 7.3.9 Reading an Entire File */
+
+
+    /* 7.3.10 Uploading Files */
+
+
+
+    /* 7.4 System Calls */
+
+
+
+    /* 7.5 XHTML or HTML5? */
+
+
+
+    /* */
+    /* */
+
+
+
+
+?>
